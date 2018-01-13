177a60d5dfc0b4924eae38a6427574cdba60f3ff
Harry Yao <harryyao13@gmail.com>
2018-01-13 15:58:55 -0800

    Added azure image recognition

---
+++ Controllers/azure-image-recognition.js
@@ -0,0 +1,63 @@
+https = require('https');
+
+module.exports = function (context, req) {
+    context.log('***************************** Start Run ****************************')
+    
+    if (req.query.imageURL || (req.body && req.body.imageURL)) {
+        var url = req.body.imageURL;
+        postContent(url, context);
+
+    } else {
+        context.res = {
+            status: 400,
+            body: "Please pass an imageURL on the query string or in the request body"
+        };
+        context.done();
+    }
+};
+
+// make request to Cognitive Services
+const postContent = function(url, context) {
+
+    var postData = JSON.stringify({'url': url});
+
+    var options = {
+        hostname: 'westcentralus.api.cognitive.microsoft.com',
+        port: 443,
+        path: '/vision/v1.0/describe',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': "15415d0ddf40416dabd7813fad8b611d"
+        }
+    };
+
+    var req = https.request(options, (res) => {
+        context.log(`STATUS: ${res.statusCode}`);
+        res.setEncoding('utf8');
+        
+        res.on('data', (chunk) => {
+            context.log(`BODY: ${chunk}`);
+            var result = JSON.parse(chunk).description.captions[0].text;
+            context.log(result);
+            context.res = {
+                body: result
+            };
+        });
+        
+        res.on('end', () => {
+            context.log('***************************** End Run ****************************');
+            context.done();
+            });
+        });
+
+        req.on('error', (e) => {
+            context.log(`problem with request: ${e.message}`);
+            context.log('***************************** End Run ****************************');
+            context.done();
+        });
+
+    // write data to request body
+    req.write(postData);
+    req.end();
+};
