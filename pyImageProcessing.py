904fd8f14388848579fd6d0c3b6960eca046387b
NotMuchToLearn <firepower789@gmail.com>
2018-01-14 02:34:09 -0800

    Added the rest of the code

--- pyImageProcessing.py
+++ pyImageProcessing.py
@@ -15,7 +15,42 @@ def find_marker(image):
  
 	# compute the bounding box of the of the paper region and return it
 	return cv2.minAreaRect(c)
-
+ 
 def distance_to_camera(knownWidth, focalLength, perWidth):
 	# compute and return the distance from the maker to the camera
 	return (knownWidth * focalLength) / perWidth
+ 
+# initialize the known distance from the camera to the object, which
+# in this case is 24 inches
+KNOWN_DISTANCE = 24.0
+ 
+# initialize the known object width, which in this case, the piece of
+# paper is 11 inches wide
+KNOWN_WIDTH = 11.0
+ 
+# initialize the list of images that we'll be using
+IMAGE_PATHS = ["images/2ft.png", "images/3ft.png", "images/4ft.png"]
+ 
+# load the furst image that contains an object that is KNOWN TO BE 2 feet
+# from our camera, then find the paper marker in the image, and initialize
+# the focal length
+image = cv2.imread(IMAGE_PATHS[0])
+marker = find_marker(image)
+focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+
+# loop over the images
+for imagePath in IMAGE_PATHS:
+	# load the image, find the marker in the image, then compute the
+	# distance to the marker from the camera
+	image = cv2.imread(imagePath)
+	marker = find_marker(image)
+	inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+ 
+	# draw a bounding box around the image and display it
+	box = np.int0(cv2.cv.BoxPoints(marker))
+	cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+	cv2.putText(image, "%.2fft" % (inches / 12),
+		(image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+		2.0, (0, 255, 0), 3)
+	cv2.imshow("image", image)
+	cv2.waitKey(0)
