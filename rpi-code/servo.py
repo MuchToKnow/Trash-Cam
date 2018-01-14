ebc014bea63fda813e1aef3d4f9011880d997834
Eric Mikulin <ericm99@gmail.com>
2018-01-14 01:47:27 +0000

    Moved file and added sleep

--- rpi-code/servo.py
+++ rpi-code/servo.py
@@ -7,8 +7,10 @@ p = GPIO.PWM(11, 50)
 
 p.start(2.5)
 p.ChangeDutyCycle(12.5)		# turn toward 180 degree
-time.sleep(10) 				# sleep 10 seconds
+time.sleep(5) 				# sleep 10 seconds
 p.ChangeDutyCycle(2.5) 		# turn toward 0 degree
+time.sleep(5)
+
 p.stop()
 
 GPIO.cleanup()
