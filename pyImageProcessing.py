e7569e6a4b4078be2e71cc5cf6d9fb6a3ae84a50
NotMuchToLearn <firepower789@gmail.com>
2018-01-14 02:30:09 -0800

    Random python image processing functions

---
+++ pyImageProcessing.py
@@ -0,0 +1,21 @@
+# import the necessary packages
+import numpy as np
+import cv2
+ 
+def find_marker(image):
+	# convert the image to grayscale, blur it, and detect edges
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (5, 5), 0)
+	edged = cv2.Canny(gray, 35, 125)
+ 
+	# find the contours in the edged image and keep the largest one;
+	# we'll assume that this is our piece of paper in the image
+	(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+	c = max(cnts, key = cv2.contourArea)
+ 
+	# compute the bounding box of the of the paper region and return it
+	return cv2.minAreaRect(c)
+
+def distance_to_camera(knownWidth, focalLength, perWidth):
+	# compute and return the distance from the maker to the camera
+	return (knownWidth * focalLength) / perWidth
