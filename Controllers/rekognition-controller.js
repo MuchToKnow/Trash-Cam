50cb457e5b1239e337c0b7a9f92fba82535a5ba0
Jeff Yang <jeffjieyang@gmail.com>
2018-01-13 18:41:19 -0800

    remove node_modules

--- Controllers/rekognition-controller.js
+++ Controllers/rekognition-controller.js
@@ -4,15 +4,17 @@ const Rekognition = require('node-rekognition')
 
 // Set your AWS credentials
 const AWSParameters = {
-    "accessKeyId": "XXX",
-    "secretAccessKey": "XXX",
-    "region": "XXX",
-    "bucket": "XXX",
-    "ACL": "XXX" // optional
+    "accessKeyId": "AKIAI7GICYUEB2SRSQUA",
+    "secretAccessKey": "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",
+    "region": "us-east-1",
+    "bucket": "trash-cam",
+  //  "ACL": "XXX" // optional
 }
 
 const rekognition = new Rekognition(AWSParameters)
 
+
+var s3Image = "currentImg.jpg"
 
 /**
  * Detects instances of real-world labels within an image
@@ -21,3 +23,5 @@ const rekognition = new Rekognition(AWSParameters)
  * @param {string} threshold (optional. Defaults 50)
  */
 const imageLabels = await rekognition.detectLabels(s3Image)
+
+console.log(imageLabels);
